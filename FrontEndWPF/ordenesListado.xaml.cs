010ab9f5e842f76cb2c5a61da433891cae538cd4
Educal72 <70333718+Educal72@users.noreply.github.com>
2024-04-01 00:45:25 -0600

    crud de ordenes completo

--- FrontEndWPF/ordenesListado.xaml.cs
+++ FrontEndWPF/ordenesListado.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using static FrontEndWPF.PuntoVenta;
 
 namespace FrontEndWPF
 {
@@ -52,18 +53,24 @@ namespace FrontEndWPF
 		private void LoadOrders()
 		{
 			Orders = new ObservableCollection<Order>
-			{
-				new Order { Id = 1, Estado = "Active", CreationTime = DateTime.Now },
-				new Order { Id = 2, Estado = "Inactive", CreationTime = DateTime.Now.AddDays(-1) },
-				new Order { Id = 3, Estado = "Active", CreationTime = DateTime.Now.AddDays(-2) },
-			};
+		{
+			new Order { Id = 1, Estado = "Pendiente", CreationTime = DateTime.Now,
+						Productos = new List<carritoItem>{ new carritoItem{Id=1, Nombre="Producto 1", Cantidad=2, Precio = 1299.00 },
+													   new carritoItem{Id=2, Nombre="Producto 2", Cantidad=1, Precio = 1399.00 }}},
+			new Order { Id = 2, Estado = "Pago", CreationTime = DateTime.Now.AddDays(-1),
+						Productos = new List<carritoItem>{ new carritoItem{Id=3, Nombre="Producto 3", Cantidad=3, Precio = 1499.00 },
+													   new carritoItem{Id=4, Nombre="Producto 4", Cantidad=4, Precio = 1599.00 }}},
+			new Order { Id = 3, Estado = "Pendiente", CreationTime = DateTime.Now.AddDays(-2),
+						Productos = new List<carritoItem>{ new carritoItem{Id = 5, Nombre="Producto 5", Cantidad=5, Precio = 1899.00 },
+													   new carritoItem{Id=6, Nombre="Producto 6", Cantidad=6, Precio = 1099.00 }}},
+		};
 
 			DataContext = this;
 		}
 
 		private void SortOrders()
 		{
-			var sortedOrders = Orders.OrderByDescending(o => o.Estado == "Active")
+			var sortedOrders = Orders.OrderByDescending(o => o.Estado == "Pendiente")
 								 .ThenByDescending(o => o.CreationTime)
 								 .ToList();
 
@@ -93,6 +100,24 @@ namespace FrontEndWPF
 				mainWindow.mainFrame.Navigate(new PuntoVenta());
 			}
 		}
+
+		private void Button_Click_3(object sender, RoutedEventArgs e)
+		{
+			Window parentWindow = Window.GetWindow(this);
+			if (parentWindow != null && parentWindow is MainWindow mainWindow)
+			{
+				mainWindow.mainFrame.Navigate(new PuntoVenta());
+			}
+		}
+
+		private void Button_Click_4(object sender, RoutedEventArgs e)
+		{
+			Window parentWindow = Window.GetWindow(this);
+			if (parentWindow != null && parentWindow is MainWindow mainWindow)
+			{
+				mainWindow.mainFrame.Navigate(new Facturacion());
+			}
+		}
 	}
 
 	public class Order
@@ -100,6 +125,15 @@ namespace FrontEndWPF
 		public int Id { get; set; }
 		public string Estado { get; set; }
 		public DateTime CreationTime { get; set; }
+		public List<carritoItem> Productos { get; set; }
+	}
+
+	public class carritoItem
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public double Precio { get; set; }
+		public int Cantidad { get; set; }
 	}
 }
 
