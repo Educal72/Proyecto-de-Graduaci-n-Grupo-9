5f9f427a96638add3457484b3773f17cbbea5c45
Educal72 <70333718+Educal72@users.noreply.github.com>
2024-06-12 21:26:01 -0600

    Solucion Merge 3

--- FrontEndWPF/Inventario/Inventario.xaml.cs
+++ FrontEndWPF/Inventario/Inventario.xaml.cs
@@ -24,6 +24,7 @@ namespace FrontEndWPF.Inventario
 		private DispatcherTimer timer;
 		List<Producto> productos = new List<Producto>();
 		Conexion conexion = new Conexion();
+		ProductosViewModel productosViewModel = new ProductosViewModel();
 		public Inventario()
 		{
 			InitializeComponent();
