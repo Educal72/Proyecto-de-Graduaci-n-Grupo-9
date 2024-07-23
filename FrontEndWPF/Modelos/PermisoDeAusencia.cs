2c29dafaa005474a1bf7f2ba6d6df4140a18a059
frgaitan24 <107448425+frgaitan24@users.noreply.github.com>
2024-07-22 22:12:55 -0600

    Modelo y view model para permiso de ausencia implementado

---
+++ FrontEndWPF/Modelos/PermisoDeAusencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEndWPF.Modelos
+{
+    public class PermisoDeAusencia
+    {
+        public int IdEmpleado { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public string Motivo { get; set; }
+        public string Estado { get; set; }
+    }
+}
